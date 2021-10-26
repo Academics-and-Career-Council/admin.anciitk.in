46665158c2eb92c4e608011d16af6bbbacc68a96
code-vaibhav <vaibhavgoyal2506@gmail.com>
2021-10-26 23:22:51 +0530

    setup type generations for apollo

---
+++ apollo.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  client: {
+    includes: ['./container/**/*.tsx', './actions/**/*.tsx', './actions/**/*.ts'],
+    tagName: 'gql',
+    service: {
+      name: "admin portal",
+      localSchemaFile: './graphql/career-schema.json'
+    }
+  }
+}
--- graphql/clients.ts
+++ graphql/clients.ts
@@ -1,21 +1,33 @@
-
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const ArgonClient = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_ARGON_URL,
-    cache: new InMemoryCache(),
-    credentials: 'allowed'
+  uri: process.env.NEXT_PUBLIC_ARGON_URL,
+  cache: new InMemoryCache(),
+  credentials: "allowed",
 });
 
-const CareerClient = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_CAREER_URL,
-    cache: new InMemoryCache(),
-    credentials: 'allowed'
-})
+export const CareerClient = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_ARGON_URL,
+  cache: new InMemoryCache(),
+  headers: {
+    "Content-Type": "application/json",
+    email: "vgoyal20@iitk.ac.in",
+  },
+  defaultOptions: {
+    query: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'all'
+    },
+    mutate: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'all'
+    }
+  },
+});
 
 const GrapQLClient = {
-    Argon: ArgonClient,
-    Career: CareerClient
-}
+  Argon: ArgonClient,
+  Career: CareerClient,
+};
 
 export default GrapQLClient;
